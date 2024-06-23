e9e3a9ab166196f3f7a13090e907d477873c4159
Micaella Melo <135082114+MicasMelo@users.noreply.github.com>
2024-06-22 21:42:23 -0300

    Atualização dos Modelos

---
+++ MODEL/usuario.php
@@ -0,0 +1,35 @@
+<?php
+    namespace MODEL;
+    class Usuario{
+        private ?int $id;
+        private ?string $usuario;
+        private ?string $senha;
+        private ?string $email;
+
+        public function __construct(){}
+
+        public function getID(){
+            return $this->id;}
+
+        public function setID(int $id){
+            $this->id = $id;}
+
+        public function getUsuario(){
+            return $this->usuario;}
+
+        public function setUsuario(string $usuario){
+            $this->usuario = $usuario;}
+
+        public function getSenha(){
+            return $this->senha;}
+
+        public function setSenha(string $senha){
+            $this->senha = $senha;}
+
+        public function getEmail(){
+            return $this->email;}
+
+        public function setEmail(string $email){
+            $this->email = $email;}
+    }
+?>
