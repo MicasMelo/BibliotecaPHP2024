6835e38ef086758674622e3a9d594d1fad9189eb
Micaella Melo <135082114+MicasMelo@users.noreply.github.com>
2024-06-24 22:33:35 -0300

    Configuração de Links no Navbar no Menu
    São os links de todas páginas de listagem e logout configuradas na barra de navegação da página inicial/menu

--- VIEW/menu.php
+++ VIEW/menu.php
@@ -6,7 +6,7 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-Br">
 <head>
 <style>
   body {
@@ -22,7 +22,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
   <!-- Compiled and minified JavaScript -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
    
   <title> Cabeceira </title>
 </head>
@@ -30,22 +30,22 @@
 <body>
 <nav class="red darken-4 z-depth-0">
   <div class="nav-wrapper container">
-    <a href="\gestaophp/VIEW/menu.php" class="brand-logo left">Cabeceira</a>
+    <a href="/gestaophp/VIEW/menu.php" class="brand-logo left">Cabeceira</a>
     <a href="#" data-target="menu-mobile" class="sidenav-trigger right"><i class="material-icons">menu</i></a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <li><a href="/gestaophp/VIEW\MEMBRO/listarLivro.php">Livros</a></li>
-      <li><a href="/gestaophp/VIEW\MEMBRO/listarMembro.php">Membros</a></li>
-      <li><a href="/gestaophp/VIEW\MEMBRO/listarEmprestimo.php">Emprestimos</a></li>
-      <li><a href="">Logout</a></li>
+      <li><a href="/gestaophp/VIEW/LIVRO/listarLivro.php">Livros</a></li>
+      <li><a href="/gestaophp/VIEW/MEMBRO/listarMembro.php">Membros</a></li>
+      <li><a href="/gestaophp/VIEW/EMPRESTIMO/listarEmpt.php">Emprestimos</a></li>
+      <li><a href="../CONTROLLER/logout.php">Logout</a></li>
     </ul>
   </div>
 </nav> 
 
 <ul id="menu-mobile" class="sidenav">
-      <li><a href="listarLivro.php">Livros</a></li>
-      <li><a href="listarMembro.php">Membros</a></li>
-      <li><a href="listarEmprestimo.php">Emprestimos</a></li>
-      <li><a href="../CONTROLLER/logout.php">Logout</a></li>
+  <li><a href="/gestaophp/VIEW/LIVRO/listarLivro.php">Livros</a></li>
+  <li><a href="/gestaophp/VIEW/MEMBRO/listarMembro.php">Membros</a></li>
+  <li><a href="/gestaophp/VIEW/EMPRESTIMO/listarEmpt.php">Emprestimos</a></li>
+  <li><a href="../CONTROLLER/logout.php">Logout</a></li>
 </ul>
 
 <!-- jQuery and Materialize JS -->
