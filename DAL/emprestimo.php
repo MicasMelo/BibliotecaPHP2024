877ded3ca8cd00d19ce13dc89d3daca2b6f10101
Micaella Melo <135082114+MicasMelo@users.noreply.github.com>
2024-06-24 22:38:39 -0300

    Correção DAL

--- DAL/emprestimo.php
+++ DAL/emprestimo.php
@@ -5,7 +5,6 @@
 
     class Emprestimo{
         public function Select(){
-
             $sql = "Select * from emprestimo;";
             $con = Conexao::conectar();
             $dados = $con->query($sql);
@@ -16,16 +15,96 @@
                 $empt->setID($linha['id']);
                 $empt->setIDLivro($linha['idLivro']);
                 $empt->setIDMembro($linha['idMembro']);
-                $empt->setData_empresimo($linha['data_emprestimo']);
+                $empt->setData_emprestimo($linha['data_emprestimo']);
                 $empt->setData_devolutiva($linha['data_devolutiva']);
                 $listaEmpt[] = $empt;
-            }
 
-            return $listaEmpt;
+                return $listaEmpt;
+            }
         }
 
-        public function SelectId(int $id){}
-        public function SelectIDLivro(string $idLivro){}
-        public function SelectIDMembro(string $idMembro){}
+        public function SelectId(int $id){
+            $sql = "Select * from emprestimo where id=?;";
+            $con = Conexao::conectar();
+            $query = $con->prepare($sql);
+            $query->execute(array($id));
+            $linha = $query->fetch(\PDO::FETCH_ASSOC);
+            $con = Conexao::desconectar();
+
+            $empt = new \MODEL\emprestimo();
+            $empt->setID($linha['id']);
+            $empt->setIDLivro($linha['idLivro']);
+            $empt->setIDMembro($linha['idMembro']);
+            $empt->setData_emprestimo($linha['data_emprestimo']);
+            $empt->setData_devolutiva($linha['data_devolutiva']);
+            
+            return $empt;
+        }
+
+        public function SelectByIdLivro(int $idLivro){
+            $sql = "Select * from emprestimo where idLivro=?;";
+            $con = Conexao::conectar();
+            $query = $con->prepare($sql);
+            $query->execute(array($idLivro));
+            $linha = $query->fetch(\PDO::FETCH_ASSOC);
+            $con = Conexao::desconectar();
+
+            $empt = new \MODEL\emprestimo();
+            $empt->setID($linha['id']);
+            $empt->setIDLivro($linha['idLivro']);
+            $empt->setIDMembro($linha['idMembro']);
+            $empt->setData_empresimo($linha['data_emprestimo']);
+            $empt->setData_devolutiva($linha['data_devolutiva']);
+            
+            return $empt;
+        }
+
+        public function SelectByIdMembro(int $idMembro){
+            $sql = "Select * from emprestimo where idMembro=?;";
+            $con = Conexao::conectar();
+            $query = $con->prepare($sql);
+            $query->execute(array($idMembro));
+            $linha = $query->fetch(\PDO::FETCH_ASSOC);
+            $con = Conexao::desconectar();
+
+            $empt = new \MODEL\emprestimo();
+            $empt->setID($linha['id']);
+            $empt->setIDLivro($linha['idLivro']);
+            $empt->setIDMembro($linha['idMembro']);
+            $empt->setData_empresimo($linha['data_emprestimo']);
+            $empt->setData_devolutiva($linha['data_devolutiva']);
+            
+            return $empt;
+        }
+
+        public function Insert(\MODEL\emprestimo $empt){
+            $sql = "INSERT INTO emprestimo (idLivro, idMembro, data_emprestimo, data_devolutiva)
+                    VALUES ('{$empt->getIDLivro()}','{$empt->getIDMembro()}','{$empt->getData_emprestimo()}','{$empt->getDAta_devolutiva()}');";
+            $con = Conexao::conectar();
+            $result = $con->query($sql);
+            $con = Conexao::desconectar();
+
+            return $result;
+        }
+
+        public function Update(\MODEL\emprestimo $empt){
+            $sql = "UPDATE emprestimo SET idLivro=?, idMembro=?, data_emprestimo=?, data_devolutiva=? WHERE id=?;";
+            $con = Conexao::conectar();
+            $query = $con->prepare($sql);
+            $result = $query->execute(array($empt->getIDLivro(),$empt->getIDMembro(),$empt->getData_emprestimo(),$empt->getData_devolutiva(),$empt->getID()));
+            $con = Conexao::desconectar();
+
+            return $result;
+        }
+
+        public function Delete(int $id){
+            $sql = "DELETE FROM emprestimo WHERE id=?;";
+            $con = Conexao::conectar();
+            $query = $con->prepare($sql);
+            $result = $query->execute(array($id));
+            $con = Conexao::desconectar();
+
+            return $result;
+        }
     }
 ?>
--- DAL/livro.php
+++ DAL/livro.php
@@ -5,7 +5,6 @@
 
     class Livro{
         public function Select(){
-
             $sql = "Select * from livro;";
             $con = Conexao::conectar();
             $dados = $con->query($sql);
@@ -19,11 +18,57 @@
                 $livro->setGenero($linha['genero']);
                 $livro->setStatus($linha['status']);
                 $listaLivro[] = $livro;
-            }
             
-            return $listaLivro;
+                return $listaLivro;
+            }
         }
 
-        public function SelectId(int $id){}
+        public function SelectId(int $id){
+            $sql = "Select * from livro where id=?;";
+            $con = Conexao::conectar();
+            $query = $con->prepare($sql);
+            $query->execute(array($id));
+            $linha = $query->fetch(\PDO::FETCH_ASSOC);
+            $con = Conexao::desconectar();
+
+            $livro = new \MODEL\livro();
+            $livro->setID($linha['id']);
+            $livro->setTitulo($linha['titulo']);
+            $livro->setAutor($linha['autor']);
+            $livro->setGenero($linha['genero']);
+            $livro->setStatus($linha['status']);
+            
+            return $livro;
+        }
+
+        public function Insert(\MODEL\livro $livro){
+            $sql = "INSERT INTO livro (titulo, autor, genero, status)
+                    VALUES ('{$livro->getTitulo()}','{$livro->getAutor()}','{$livro->getGenero()}','{$livro->getStatus()}');";
+            $con = Conexao::conectar();
+            $result = $con->query($sql);
+            $con = Conexao::desconectar();
+
+            return $result;
+        }
+
+        public function Update(\MODEL\livro $livro){
+            $sql = "UPDATE livro SET titulo=?, autor=?, genero=?, status=? WHERE id=?;";
+            $con = Conexao::conectar();
+            $query = $con->prepare($sql);
+            $result = $query->execute(array($livro->getTitulo(),$livro->getAutor(),$livro->getGenero(),$livro->getStatus(),$livro->getID()));
+            $con = Conexao::desconectar();
+
+            return $result;
+        }
+
+        public function Delete(int $id){
+            $sql = "DELETE FROM livro WHERE id=?;";
+            $con = Conexao::conectar();
+            $query = $con->prepare($sql);
+            $result = $query->execute(array($id));
+            $con = Conexao::desconectar();
+
+            return $result;
+        }
     }
 ?>
